e79ff26fac07405aa95a6ff9278910cdfed61962
qwark97 <marcin.plata1.konta@gmail.com>
2021-11-16 20:40:10 +0100

    prepare sketch of the solution

--- run.py
+++ run.py
@@ -1,19 +1,36 @@
 from data_reader import read_file, setup_model
 import sys
+from random import uniform
 
-from data_structs import Solution
-from utils import generate_initial_solution
+from utils import generate_initial_solution, epsilon, fitness
 
 def start():
+    TEMP = 100
+    COOLING_FACTOR = 0.9
+    EPOCHS = 1000
+    ATTEMPTS = 500
+    
     try:
         file_name = sys.argv[1]
     except IndexError:
         file_name = 'input.json'
     data = read_file(file_name)
     model = setup_model(data)
-    
-    solution = Solution()
-    solution.nodes_seq = generate_initial_solution(model.node_seq_no_list)
+
+    best_list_of_path = generate_initial_solution(model)
+
+    for _ in range(EPOCHS):
+        for _ in range(ATTEMPTS):
+
+            candidate_list_of_paths = [[]]
+            
+            if fitness(candidate_list_of_paths) < fitness(best_list_of_path):
+                best_list_of_path = candidate_list_of_paths
+
+            elif uniform(0, 1) < epsilon(candidate_list_of_paths, best_list_of_path, TEMP):
+                best_list_of_path = candidate_list_of_paths
+
+        TEMP *= COOLING_FACTOR
     
 
 if __name__ == '__main__':
