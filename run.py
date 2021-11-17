f943a3b1978a1cb677c20dc134e47d315fa4407e
qwark97 <marcin.plata1.konta@gmail.com>
2021-11-17 21:13:44 +0100

    add to input.js parameters of the algorithm

--- run.py
+++ run.py
@@ -1,50 +1,54 @@
 from data_reader import read_file, setup_model
 import sys
-from random import random, uniform, randint, seed
+from random import uniform, randint
+import random
 
 from utils import generate_initial_solution, prepare_solution
 
+
 def start():
-    TEMP = 100
-    COOLING_FACTOR = 0.90
-    EPOCHS = 1000
-    ATTEMPTS = 50
-    MINIMAL_TEMP = 5
-    seed(0)
-    
     try:
         file_name = sys.argv[1]
     except IndexError:
         file_name = 'input.json'
     data = read_file(file_name)
-    model = setup_model(data)
 
+    if not data['parameters']['random_seed']:
+        random.seed(0)
+
+    epochs = data['parameters']['epochs']
+    attempts = data['parameters']['attempts']
+    minimal_temp = data['parameters']['minimal_temp']
+    cooling_factor = data['parameters']['cooling_factor']
+    temp = data['parameters']['initial_temperature']
+
+    model = setup_model(data)
     model.current_best_solution = generate_initial_solution(model)
     model.first_solution = model.current_best_solution
 
-    for _ in range(EPOCHS):
-        for _ in range(ATTEMPTS):
+    for _ in range(epochs):
+        for _ in range(attempts):
             strategy = randint(0, 1)
             candidate_list_of_paths = prepare_solution(model, strategy)
             
             if model.fitness(candidate_list_of_paths) < model.fitness(model.current_best_solution):
                 model.current_best_solution = candidate_list_of_paths
 
-            elif uniform(0, 1) < model.epsilon(candidate_list_of_paths, model.current_best_solution, TEMP):
+            elif uniform(0, 1) < model.epsilon(candidate_list_of_paths, model.current_best_solution, temp):
                 model.current_best_solution = candidate_list_of_paths
             
             print('.', sep='', end='', flush=True)
         
-        if TEMP <= MINIMAL_TEMP:
+        if temp <= minimal_temp:
             break
-        TEMP *= COOLING_FACTOR
-        print('current temp:', TEMP)
+        temp *= cooling_factor
+        print('current temp:', temp)
     
     print()
     print('first solution:', model.fitness(model.first_solution))
     print('current best distance:', model.fitness(model.current_best_solution))
     print('best known distance:', model.fitness(model.best_known_solution))
-    print('TEMP:', TEMP)
+    print('temp:', temp)
     
 
 if __name__ == '__main__':
